c3db03b5728c934a0362e7509772d86bafc7fad8
Stiliyan <stiliqnskiq.st@abv.bg>
2023-05-25 08:03:04 +0300

    added Angular Material

--- article-discussion-app/src/app/app.module.ts
+++ article-discussion-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { CoreModule } from './modules/core/core.module';
@@ -12,7 +13,8 @@ import { SharedModule } from './modules/shared/shared.module';
   imports: [
     BrowserModule,
     CoreModule,
-    SharedModule
+    SharedModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
